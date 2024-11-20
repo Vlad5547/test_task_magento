c4254d7d775efcc0cd9a07841c695f4a5b307605
Vlad <goncarvlad2004@gmail.com>
2024-11-20 13:08:18 +0000

    fix scripts

--- src/app/design/frontend/Vendor/theme/requirejs-config.js
+++ src/app/design/frontend/Vendor/theme/requirejs-config.js
@@ -1,10 +1,22 @@
 var config = {
-    paths: {            
-        'owlcarousel': "Magento_Theme/js/owl.carousel.min"
-    },   
+    paths: {
+        'owlcarousel': "Magento_Theme/js/owl.carousel.min",
+        'customScript': "Magento_Theme/js/custom",
+        'tabsScript': "Magento_Theme/js/tabs_script",
+        'popupScript': "Magento_Theme/js/popup"
+    },
     shim: {
         'owlcarousel': {
             deps: ['jquery']
-        }
+        },
+        'customScript': {
+            deps: ['jquery']
+        },
+        'tabsScript': {
+            deps: ['jquery']
+        },
+        'popupScript': {
+            deps: ['jquery']
+        },
     }
 };
