e1cf6f75bee09e95798a169ca18bd626b97ef6e9
Vlad <goncarvlad2004@gmail.com>
2024-11-20 12:54:33 +0000

    fix scripts

--- src/app/design/frontend/Vendor/theme/Magento_Theme/web/js/custom.js
+++ src/app/design/frontend/Vendor/theme/Magento_Theme/web/js/custom.js
@@ -1,22 +1,18 @@
-console.log('hello world!');
-
 require(["jquery", "owlcarousel"], function($) {
-    $(document).ready(function(){
-        $(".owl-carousel").owlCarousel({
-            items: 4,
-            loop: true,
-            margin: 10,
-            responsive: {
-                0: {
-                    items: 2
-                },
-                600: {
-                    items: 2
-                },
-                1000: {
-                    items: 4
-                }
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
             }
-        });
+        }
     });
 });
--- src/app/design/frontend/Vendor/theme/requirejs-config.js
+++ src/app/design/frontend/Vendor/theme/requirejs-config.js
@@ -1,9 +1,17 @@
 var config = {
-    paths: {            
-        'owlcarousel': "Magento_Theme/js/owl.carousel.min"
-    },   
+    paths: {
+        'owlcarousel': "Magento_Theme/js/owl.carousel.min",
+        'customScript': "Magento_Theme/js/custom",
+        'tabsScript': "Magento_Theme/js/tabs_script"
+    },
     shim: {
         'owlcarousel': {
+            deps: ['jquery']
+        },
+        'customScript': {
+            deps: ['jquery']
+        },
+        'tabsScript': {
             deps: ['jquery']
         }
     }
